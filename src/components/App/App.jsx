3cc04db4bf0aee45c2e59be3ba7e161eea859653
ilya9293 <ilya9293@ukr.net>
2022-06-29 12:53:07 +0300

    add value input

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,13 +7,28 @@ class App extends Component {
     name: '',
   };
 
-  handleChange = e => {};
+  handleChange = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleSubmit = e => {
+    const { name } = this.state;
+    e.preventDefault();
+    this.setState(prevState => ({ contacts: [...(prevState.contacts + name)] }));
+    this.reset();
+    console.log(this.state.contacts);
+  };
+
+  reset = () => {
+    this.setState({ name: ' ' });
+  };
 
   render() {
+    const { name } = this.state;
     return (
       <div className={s.container}>
         <h2 className={s.title}>Phonebook</h2>
-        <form className={s.form}>
+        <form className={s.form} onSubmit={this.handleSubmit}>
           <label>
             Name
             <input
@@ -22,6 +37,7 @@ class App extends Component {
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
+              value={name}
               onChange={this.handleChange}
             />
           </label>
